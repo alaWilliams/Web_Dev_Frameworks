32688d5a7ed99b7318c34cee2a4f9faf9210625a
Ala Williams <alicja.m.williams@gmail.com>
2025-09-21 18:14:11 +0300

    starting on Set1

---
+++ exercises/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Exercise1 from './Set1/exercise1'
+
+import './App.css'
+
+
+
+function App() {
+
+ 
+  return (
+    <Exercise1 />
+    
+  )
+}
+
+export default App
---
+++ exercises/src/Set1/exercise1.tsx
@@ -0,0 +1,7 @@
+// Exercise 1: Render a <h1> heading with the text "Hello, React!" using JSX.
+
+export default function Exercise1() {
+  return (
+    <h1>Hello, React!</h1>
+  )
+}
